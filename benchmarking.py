6c0e9eed45b83677292741b3061e51507217a695
samejima <samejima@ist.osaka-u.ac.jp>
2017-09-08 09:31:27 +0900

    add test code

--- benchmarking.py
+++ benchmarking.py
@@ -3,6 +3,13 @@ Created on 2017/09/05
 
 @author: Masaki Samejima
 '''
+import numpy
+import scipy.sparse.linalg
+import sparseqr
 
-if __name__ == '__main__':
-    pass
+if __name__ == '__main__':    
+    # QR decompose a sparse matrix M such that  Q R = M E
+    #
+    M = scipy.sparse.rand( 10, 10, density = 0.1 )
+    Q, R, E, rank = sparseqr.qr( M )
+    print( abs( Q*R - M*sparseqr.permutation_vector_to_matrix(E) ).sum() )  # should be approximately zero
