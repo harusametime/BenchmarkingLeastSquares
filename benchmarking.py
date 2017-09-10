e6277bb91cf0ace1a00bdf52dbe414a97df5f1c8
Masaki Samejima <samejima@ist.osaka-u.ac.jp>
2017-09-11 04:08:04 +0900

    Add code of solving by PySPQR

--- benchmarking.py
+++ benchmarking.py
@@ -3,13 +3,29 @@ Created on 2017/09/05
 
 @author: Masaki Samejima
 '''
-import numpy
+import numpy as np
 import scipy.sparse.linalg
 import sparseqr
+import time
 
 if __name__ == '__main__':    
-    # QR decompose a sparse matrix M such that  Q R = M E
+
+    n_trials = 10
+    
+    matrix_size = np.array([2000,1000])
+    matrix_density = 0.1
+
+    # Solve an overdetermined linear system  A x = b  in the least-squares sense
     #
-    M = scipy.sparse.rand( 10, 10, density = 0.1 )
-    Q, R, E, rank = sparseqr.qr( M )
-    print( abs( Q*R - M*sparseqr.permutation_vector_to_matrix(E) ).sum() )  # should be approximately zero
+    # The same routine also works for the usual non-overdetermined case.
+    #
+    total_time = 0
+    for i in range(n_trials):
+        A = scipy.sparse.rand(matrix_size[0],matrix_size[1], density = matrix_density )  
+        b = np.random.random(matrix_size[0])  
+        start = time.clock()
+        x = sparseqr.solve( A, b, tolerance = 0 )    
+        end = time.clock()
+        total_time += end-start
+    
+    print total_time/float(n_trials), "sec."
